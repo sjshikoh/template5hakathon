1c49edcb5cc76df29e4b64676f645341165b62d3
sjshikoh <148519226+sjshikoh@users.noreply.github.com>
2024-12-08 22:49:25 +0500

    bugfixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import Carosel from "@/components/carosel";
-import Image from "next/image";
-import Button from '@/components/button';
+import Button from "@/components/button";
 import Titlebar from "@/components/Titlebar";
-
 
 export default function Home() {
   return (
-   <div>
-     
-   <Carosel />
-   <Button />
-   <Titlebar />
-   
-   </div>
+    <div>
+      <Carosel />
+      <Button />
+      <Titlebar />
+    </div>
   );
 }
